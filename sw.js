884dd0c9fa4dc65e332d5ef313d7e053888a8b9f
Robert <krstb-90@yahoo.com>
2026-01-03 13:55:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'uhrzeit-v1';
+const CACHE_NAME = 'uhrzeit-v2';
 const ASSETS = [
   'index.html',
   'manifest.json',
@@ -44,4 +44,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-
