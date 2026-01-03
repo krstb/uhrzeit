323183db4e4a09c395e3ae0570c512a0ecf692d4
Robert <krstb-90@yahoo.com>
2026-01-03 13:45:19 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'uhrzeit'; // Ändere v2 zu v3, v4 etc., um Updates zu erzwingen
+const CACHE_NAME = 'notizen offline'; // Ändere v2 zu v3, v4 etc., um Updates zu erzwingen
 const ASSETS = [
   'index.html',
   'manifest.json'
